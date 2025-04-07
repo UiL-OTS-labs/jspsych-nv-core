996a7964de981450f783df12c1feb08c174cff94
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-04-07 11:53:44 +0200

    Forward from pn to ld

--- pn/main.js
+++ pn/main.js
@@ -208,6 +208,14 @@ let end_screen = {
     // trial_duration: DEBRIEF_MESSAGE_DURATION,
     on_load: function (){
         uil.saveData();
+        if (redirection_params.do_ld) {
+            let current_url = new URL (window.location.href);
+            let new_url = new URL("../ld", current_url);
+            for ([key, value] of current_url.searchParams.entries()) {
+                new_url.searchParams.set(key, value);
+            }
+            window.location.replace(new_url);
+        }
     },
     on_finish : function(data) {
         if (typeof data.rt === "number") {
