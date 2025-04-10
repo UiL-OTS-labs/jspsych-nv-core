77fb9ccf0b4813ba832102261ffb410237c478ee
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-04-10 09:57:15 +0200

    Improve extracting the audio from data files

--- pn/output/extract_audio.py
+++ pn/output/extract_audio.py
@@ -8,6 +8,8 @@ import json
 import sys
 import ffmpeg
 import tempfile
+import os.path as p
+
 from argparse import ArgumentParser
 
 PathLike = typing.Union[str, bytes, os.PathLike]
@@ -22,19 +24,26 @@ def getJspychOutput(fn: PathLike) -> list[dict]:
 def extract_audio_blocks(fn: PathLike, format: str):
     jsdata = getJspychOutput(fn)
     key = "base64_audio"
+    pp_id = "pp_id"
     audio_trials = list(filter(lambda trial: key in trial, jsdata))
 
     for i, trial in enumerate(audio_trials):
         print("decoding")
         base64str = trial[key]
 
-        outname = f"audio_block{i+1}.{format}"
+        dir_name = trial[pp_id]
+        if p.exists(dir_name):
+            if not p.isdir(dir_name):
+                exit(f"Oops: {dir_name} exists but it isn't a folder")
+        else:
+            os.mkdir(dir_name)
+
+        outname = f"{dir_name}/audio_block{i+1}.{format}"
 
         # create temp decoded base64 trial
         with tempfile.NamedTemporaryFile() as tmpfile:
             tmpfile.write(base64.b64decode(base64str))
             tmpfile.flush()
-            print(tmpfile.name)
             (ffmpeg.input(tmpfile.name).output(outname).run())
 
 
